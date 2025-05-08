b2f314f2f95a0f9f0f7f88ab47028ea5a1d2899c
JOEL_ALBERTO_PEREA_CASTORENA <a.22308051281272@cbtis128.edu.mx>
2025-05-08 21:13:48 +0000

    Diseño Flutter

--- lib/main.dart
+++ lib/main.dart
@@ -1,41 +1,238 @@
+import 'package:PereaFlutter/Animations/FadeAnimation.dart';
 import 'package:flutter/material.dart';
 
-void main() => runApp(MyApp());
+void main() => runApp(MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: HomePage(),
+    ));
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-  // This widget is the root of your application.
+class HomePage extends StatefulWidget {
   @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      // Application name
-      title: 'Flutter Hello World',
-      // Application theme data, you can set the colors for the application as
-      // you want
-      theme: ThemeData(
-        // useMaterial3: false,
-        primarySwatch: Colors.blue,
-      ),
-      // A widget which will be started on application startup
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
-    );
-  }
+  _HomePageState createState() => _HomePageState();
 }
 
-class MyHomePage extends StatelessWidget {
-  final String title;
-  const MyHomePage({super.key, required this.title});  
-
+class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        // The title text which will be shown on the action bar
-        title: Text(title),
+      backgroundColor: Colors.black,
+      body: Stack(
+        children: <Widget>[
+          CustomScrollView(
+            slivers: <Widget>[
+              SliverAppBar(
+                expandedHeight: 450,
+                backgroundColor: Colors.black,
+                flexibleSpace: FlexibleSpaceBar(
+                  collapseMode: CollapseMode.pin,
+                  background: Container(
+                    decoration: BoxDecoration(
+                        image: DecorationImage(
+                            image:
+                                AssetImage('assets/images/Tybw_Yamamoto.gif'),
+                            fit: BoxFit.cover)),
+                    child: Container(
+                      decoration: BoxDecoration(
+                          gradient: LinearGradient(
+                              begin: Alignment.bottomRight,
+                              colors: [
+                            Colors.black,
+                            Colors.black.withOpacity(.3)
+                          ])),
+                      child: Padding(
+                        padding: EdgeInsets.all(20),
+                        child: Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          mainAxisAlignment: MainAxisAlignment.end,
+                          children: <Widget>[
+                            FadeAnimation(
+                                1,
+                                Text(
+                                  "Shigekuni Yamamoto Genryūsai",
+                                  style: TextStyle(
+                                      color: Colors.white,
+                                      fontWeight: FontWeight.bold,
+                                      fontSize: 40),
+                                )),
+                            SizedBox(
+                              height: 20,
+                            ),
+                            Row(
+                              children: <Widget>[
+                                FadeAnimation(
+                                    1.2,
+                                    Text(
+                                      "60 Videos",
+                                      style: TextStyle(
+                                          color: Colors.grey, fontSize: 16),
+                                    )),
+                                SizedBox(
+                                  width: 50,
+                                ),
+                                FadeAnimation(
+                                    1.3,
+                                    Text(
+                                      "240K Subscribers",
+                                      style: TextStyle(
+                                          color: Colors.grey, fontSize: 16),
+                                    ))
+                              ],
+                            )
+                          ],
+                        ),
+                      ),
+                    ),
+                  ),
+                ),
+              ),
+              SliverList(
+                delegate: SliverChildListDelegate([
+                  Padding(
+                    padding: EdgeInsets.all(20),
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: <Widget>[
+                        FadeAnimation(
+                            1.6,
+                            Text(
+                              "Shigekuni Yamamoto Genryūsai (元柳斎山本重國, Genryūsai Yamamoto Shigekuni) fue el Capitán de la 1ª División del Gotei 13, así como era el Comandante General del Gotei 13. Su teniente fue Chōjirō Sasakibe. Actualmente Yamamoto es un residente del Infierno y lleva el Epíteto de Gotei Kaiso (護廷開祖, Fundador del Gotei).",
+                              style: TextStyle(color: Colors.grey, height: 1.4),
+                            )),
+                        SizedBox(
+                          height: 40,
+                        ),
+                        FadeAnimation(
+                            1.6,
+                            Text(
+                              "Born",
+                              style: TextStyle(
+                                  color: Colors.white,
+                                  fontSize: 18,
+                                  fontWeight: FontWeight.bold),
+                            )),
+                        SizedBox(
+                          height: 10,
+                        ),
+                        FadeAnimation(
+                            1.6,
+                            Text(
+                              "21 de Enero, Soul Society e",
+                              style: TextStyle(color: Colors.grey),
+                            )),
+                        SizedBox(
+                          height: 20,
+                        ),
+                        FadeAnimation(
+                            1.6,
+                            Text(
+                              "Nationality",
+                              style: TextStyle(
+                                  color: Colors.white,
+                                  fontSize: 18,
+                                  fontWeight: FontWeight.bold),
+                            )),
+                        SizedBox(
+                          height: 10,
+                        ),
+                        FadeAnimation(
+                            1.6,
+                            Text(
+                              "Shinigami",
+                              style: TextStyle(color: Colors.grey),
+                            )),
+                        SizedBox(
+                          height: 20,
+                        ),
+                        FadeAnimation(
+                            1.6,
+                            Text(
+                              "Videos",
+                              style: TextStyle(
+                                  color: Colors.white,
+                                  fontSize: 18,
+                                  fontWeight: FontWeight.bold),
+                            )),
+                        SizedBox(
+                          height: 20,
+                        ),
+                        FadeAnimation(
+                            1.8,
+                            Container(
+                              height: 200,
+                              child: ListView(
+                                scrollDirection: Axis.horizontal,
+                                children: <Widget>[
+                                  makeVideo(
+                                      image:
+                                          'assets/images/yamamoto_shigekuni.gif'),
+                                  makeVideo(
+                                      image: 'assets/images/yamamoto.gif'),
+                                  makeVideo(
+                                      image:
+                                          "assets/images/yamamoto_bleach.gif"),
+                                ],
+                              ),
+                            )),
+                        SizedBox(
+                          height: 120,
+                        )
+                      ],
+                    ),
+                  )
+                ]),
+              )
+            ],
+          ),
+          Positioned.fill(
+            bottom: 50,
+            child: Container(
+              child: Align(
+                alignment: Alignment.bottomCenter,
+                child: FadeAnimation(
+                  2,
+                  Container(
+                    margin: EdgeInsets.symmetric(horizontal: 30),
+                    height: 50,
+                    decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(50),
+                        color: Colors.yellow[700]),
+                    child: Align(
+                        child: Text(
+                      "Bankai",
+                      style: TextStyle(color: Colors.white),
+                    )),
+                  ),
+                ),
+              ),
+            ),
+          )
+        ],
       ),
-      body: Center(
-        child: Text(
-          'Hello, World!',
+    );
+  }
+
+  Widget makeVideo({image}) {
+    return AspectRatio(
+      aspectRatio: 1.5 / 1,
+      child: Container(
+        margin: EdgeInsets.only(right: 20),
+        decoration: BoxDecoration(
+            borderRadius: BorderRadius.circular(20),
+            image:
+                DecorationImage(image: AssetImage(image), fit: BoxFit.cover)),
+        child: Container(
+          decoration: BoxDecoration(
+              gradient: LinearGradient(begin: Alignment.bottomRight, colors: [
+            Colors.black.withOpacity(.9),
+            Colors.black.withOpacity(.3)
+          ])),
+          child: Align(
+            child: Icon(
+              Icons.play_arrow,
+              color: Colors.white,
+              size: 70,
+            ),
+          ),
         ),
       ),
     );
